cea9d2ed94b88f3c05f016661f81baa735194985
qwg2200 <qwg2200@naver.com>
2024-04-26 18:14:42 +0900

    게시글 상세 API 수정

--- src/controllers/post/post.controller.ts
+++ src/controllers/post/post.controller.ts
@@ -52,10 +52,12 @@ const getBlogPost = async (req: Request, res: Response) => {
 
 const getBlogPostById = async (req: Request, res: Response) => {
     try {
+        const { id } = req.params;
+
+        const postId = Number(id);
+
         const postById = await prisma.post.findUnique({
-            where: {
-                id: 1,
-            },
+            where: { id: postId },
             include: {
                 user: {
                     select: {
